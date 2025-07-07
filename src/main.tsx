113baffb6f1332ed70acacc969f8fc1c211bb8cf
Tom DeAbreu <tomdeabreu@proton.me>
2025-07-07 22:10:09 +0100

    Add posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { PostHogProvider } from "posthog-js/react";
+import type { ConfigDefaults } from "posthog-js";
 
-createRoot(document.getElementById('root')!).render(
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24" as ConfigDefaults,
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
+  </StrictMode>
+);
