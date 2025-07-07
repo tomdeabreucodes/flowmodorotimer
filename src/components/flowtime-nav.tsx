5d57c056552448c3b14e7b90234f564f935d0eb1
Tom DeAbreu <tomdeabreu@proton.me>
2025-07-07 20:46:15 +0100

    correct nav padding

--- src/components/flowtime-nav.tsx
+++ src/components/flowtime-nav.tsx
@@ -7,7 +7,7 @@ import { IoHourglass } from "react-icons/io5";
 
 export function FlowtimeNavigationMenu({ settings }: settingsType) {
   return (
-    <NavigationMenu className="mb-4 max-w-none justify-between p-4">
+    <NavigationMenu className="mb-4 max-w-none justify-between py-4">
       <div className="flex items-center space-x-3">
         <div className="w-10 h-10 bg-gradient-to-r to-cerulean-800 from-cerulean-200 rounded-lg">
           <IoHourglass className="w-full h-full text-cerulean-50 p-2" />{" "}
