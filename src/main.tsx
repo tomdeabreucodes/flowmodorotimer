e1d1e5adb7039aee7f08e46d7caa3e9dabd55641
Tom DeAbreu <tomdeabreu@proton.me>
2025-07-07 23:09:03 +0100

    revert posthog in favour of cloudflare for simpler web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { PostHogProvider } from "posthog-js/react";
-import type { ConfigDefaults } from "posthog-js";
-
-const options = {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  defaults: "2025-05-24" as ConfigDefaults,
-};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
-      options={options}
-    >
-      <App />
-    </PostHogProvider>
+    <App />
   </StrictMode>
 );
